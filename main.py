2fa63e058e5db0f0aae1420394b005f0efce5bdd
SyedVkax <syed.vkax@gmail.com>
2022-02-16 13:57:16 +0500

    First Commit

--- main.py
+++ main.py
@@ -29,7 +29,11 @@ while game_is_on:
     # Detect collision with the wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         # Needs to bounce
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with the r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
 screen.exitonclick()
